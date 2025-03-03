0a1c1e309e5eb302b4021a0e3036f80fde9f7af3
169404 <169404@cloud.rangitoto.school.nz>
2025-03-03 13:25:14 +1300

    Created ball functions.

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import random
 
 
 class GUI:
@@ -14,17 +13,16 @@ class GUI:
         self.title = self.canvas.create_text(self.CANVAS_WIDTH/2, 50,
                                              text="PONG", fill="white")
 
-        self.canvas.bind("<w>", self.player_one_up)
+        self.canvas.bind("<Button-1>", self.player_one_up)
 
-        # self.left = Paddle()
-
+        self.left = Paddle()
         self.window.mainloop()
 
-
     def player_one_up(self):
-        self.canvas.create_rectangle(10, 10, 10 + 100, 10 + 100, fill=f""
-                                                             f"#{random.randint(0, 0xFFFFFF):06x}")
-        # self.left.MoveUp()
+        self.left.MoveUp()
+
+    def gain_point(self):
+        pass
 
 
 
@@ -34,7 +32,7 @@ class Paddle:
         self.canvas = Canvas(width=10, height= 75, background="white")
         self.y = y_vel
     def move_up(self):
-        pass
+
 
     def move_down(self):
         pass
@@ -46,7 +44,20 @@ class Paddle:
         pass
 
 class Ball:
-    pass
+    def __init__(self):
+        self.canvas = Canvas
+
+    def appear_random(self):
+        pass
+
+    def move_ball(self):
+        pass
+
+    def hit_paddle(self):
+        pass
+
+    def hit_wall(self):
+        pass
 
 
 GUI()
