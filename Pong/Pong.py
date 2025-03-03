7e07b2b37945f612eb50f5de4c83e1a2c5b02b8b
Kate <kate.a.blackett@gmail.com>
2025-03-03 13:16:59 +1300

    gui stuff

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 
 class GUI:
@@ -12,14 +13,18 @@ class GUI:
         self.canvas.pack()
         self.title = self.canvas.create_text(self.CANVAS_WIDTH/2, 50,
                                              text="PONG", fill="white")
+
+        self.canvas.bind("<w>", self.player_one_up)
+
+        # self.left = Paddle()
+
         self.window.mainloop()
 
-        self.canvas.bind("<Button-1>", self.player_1_up)
-
-        self.left = Paddle()
 
     def player_one_up(self):
-        self.left.MoveUp()
+        self.canvas.create_rectangle(10, 10, 10 + 100, 10 + 100, fill=f""
+                                                             f"#{random.randint(0, 0xFFFFFF):06x}")
+        # self.left.MoveUp()
 
 
 
@@ -29,7 +34,7 @@ class Paddle:
         self.canvas = Canvas(width=10, height= 75, background="white")
         self.y = y_vel
     def move_up(self):
-
+        pass
 
     def move_down(self):
         pass
